4da9ddefe4696055af9cfe58ab4f57f00fa5e7f1
dmtrmkhlv <app@dmtrmkhlv.ru>
2022-05-25 16:30:19 +0300

    add log

--- hw04.js
+++ hw04.js
@@ -1,60 +1,72 @@
-// const fs = require("fs");
-// const colors = require("colors/safe");
-// const yargs = require("yargs");
-// const path = require("path");
-
-// const options = yargs
-//     .usage("Usage: -p <path>")
-//     .option("p", {
-//         alias: "path",
-//         describe: "Path to file",
-//         type: "string",
-//         demandOption: true
-//     })
-//     .argv;
-
-// const filePath = path.join(__dirname, options.path);
-
-// fs.readFile(filePath, 'utf8', (err, data) => {
-//     console.log(colors.cyan(data));
-// });
-
-// const fs = require("fs");
-// const path = require("path");
-// const readline = require("readline");
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-// rl.question("Please enter the path to the file: ", function (inputedPath) {
-//     const filePath = path.join(__dirname, inputedPath);
-//     fs.readFile(filePath, 'utf8', (err, data) => {
-//         console.log(data);
-//         rl.close();
-//     });
-// });
-// rl.on("close", function () {
-//     process.exit(0);
-// });
-
 const fs = require("fs");
+const {Transform} = require('stream');
+const colors = require("colors/safe");
 const path = require("path");
+const readline = require("readline");
 const inquirer = require("inquirer");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 const isFile = fileName => {
     return fs.lstatSync(fileName).isFile();
 }
-const list = fs.readdirSync(__dirname).filter(isFile);
-inquirer
-    .prompt([{
-        name: "fileName",
-        type: "list",
-        message: "Choose file:",
-        choices: list,
-    }])
-    .then((answer) => {
-        console.log(answer.fileName);
-        const filePath = path.join(__dirname, answer.fileName);
-        fs.readFile(filePath, 'utf8', (err, data) => {
-            console.log(data);
-        });
+
+const findStr = (str, filterStr) => {
+    let arrOfStr = str.toString().split("\n");
+    return arrOfStr.filter(function (s) {
+        return s.match(filterStr);
+    }).join("\n");
+}
+
+const createStream = (readStream, filterStr) => {
+    const transformStream = new Transform({
+        transform(chunk, encoding, callback) {
+            const transformedChunk = findStr(chunk, filterStr);
+            callback(null, transformedChunk);
+        }
     });
+    readStream.pipe(transformStream).pipe(process.stdout);
+}
+
+const checkIsFile = (dirInput) => {
+    console.log(dirInput);
+    // const list = fs.readdirSync(dirInput).map((arg) => {
+    //     if (isFile(arg)) {
+    //         return arg;
+    //     } else {
+    //         return arg;
+    //     }
+    // });
+
+    const list = fs.readdirSync(dirInput).filter(isFile);
+
+    inquirer
+        .prompt([{
+            name: "fileName",
+            type: "list",
+            message: "Выберите файл:",
+            choices: list,
+        }])
+        .then((answer) => {
+            if (isFile(answer.fileName)) {
+                console.log("is file");
+                const readStream = new fs.ReadStream(answer.fileName, 'utf8');
+                createStream(readStream, "89.123.1.41");
+                rl.close();
+            } else {
+                const filePath = path.join("./", answer.fileName);
+                // checkIsFile(filePath);
+                console.log(filePath);
+                // checkIsFile(answer.fileName);
+                // console.log("is dir");
+            }
+        });
+}
+
+rl.question(colors.red("Напишите директорию: "), function (inputedPath) {
+    checkIsFile(inputedPath);
+});
+rl.on("close", function () {
+    process.exit(0);
+});
